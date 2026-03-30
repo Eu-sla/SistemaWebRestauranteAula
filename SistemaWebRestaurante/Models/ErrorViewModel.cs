262f979805bddd4bae2a61943befe480295c4e0f
isabe <isabe@MEU>
2026-03-30 15:55:25 -0300

    Adicionar arquivos de projeto.

---
+++ SistemaWebRestaurante/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace SistemaWebRestaurante.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
