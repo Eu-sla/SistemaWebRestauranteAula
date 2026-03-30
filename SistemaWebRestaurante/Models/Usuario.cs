9aa6cbf95dc013bff24ccfc1d69fcd91f9c6e538
isabe <isabe@MEU>
2026-03-30 16:44:56 -0300

    login funcionando

---
+++ SistemaWebRestaurante/Models/Usuario.cs
@@ -0,0 +1,9 @@
+﻿namespace SistemaWebRestaurante.Models
+{
+    public class Usuario
+    {
+        public int Id { get; set; }
+        public string Email { get; set; }
+        public string Senha { get; set; }
+    }
+}
